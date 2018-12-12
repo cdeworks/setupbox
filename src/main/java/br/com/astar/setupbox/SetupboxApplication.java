bd1725c618f01cdeaae87b3b41dc47c16c71f90f
Daniel Reis <dreis2003@hotmail.com>
2018-12-12 10:39:20 -0200

    Commit Inicial

---
+++ src/main/java/br/com/astar/setupbox/SetupboxApplication.java
@@ -0,0 +1,14 @@
+package br.com.astar.setupbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetupboxApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/astar/setupbox/SetupboxApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.astar.setupbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SetupboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
