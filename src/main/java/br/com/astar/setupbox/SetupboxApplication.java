0cc230b1a14f9599dac55c922bd4fa86445f8dc0
Daniel Reis <dreis2003@hotmail.com>
2018-12-26 08:51:28 -0200

    Ajuste para Spring reconhecer metodos assincronos

--- src/main/java/br/com/astar/setupbox/SetupboxApplication.java
+++ src/main/java/br/com/astar/setupbox/SetupboxApplication.java
@@ -2,12 +2,25 @@ package br.com.astar.setupbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+@EnableAsync
 @SpringBootApplication
 public class SetupboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SetupboxApplication.class, args);
+	}
+	
+	@Bean(name = "fileExecutor")
+	public ThreadPoolTaskExecutor taskExecutor() {
+		final ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+		pool.setCorePoolSize(1);
+		pool.setMaxPoolSize(3);
+		pool.initialize();
+		return pool;
 	}
 
 }
