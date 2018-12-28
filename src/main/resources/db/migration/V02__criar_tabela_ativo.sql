6ec2e6531aebeb7cc8bde04229bad41b5e7d536b
Daniel Reis <dreis2003@hotmail.com>
2018-12-28 15:57:29 -0200

    Script para criação da tabela Ativos

---
+++ src/main/resources/db/migration/V02__criar_tabela_ativo.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ativos (
+	id 				BIGINT(20) 		PRIMARY KEY AUTO_INCREMENT,
+	tipo			VARCHAR(50),
+	modelo			VARCHAR(50),
+	fabricante		VARCHAR(50), 
+	serial_number	VARCHAR(50),
+	cad_id			VARCHAR(50),
+	cm_mac			VARCHAR(50),
+	emta_mac		VARCHAR(50),
+	estado			VARCHAR(50),
+	tipo_defeito	VARCHAR(50),
+	localizacao		VARCHAR(50)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+	
