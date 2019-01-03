bbe4830728b2929264fd272a930fd35a0a00d3e1
Daniel Reis <dreis2003@hotmail.com>
2019-01-03 17:18:11 -0200

    Anotação do spring que estava faltando

--- src/main/java/br/com/astar/setupbox/domain/repository/AtivoRepository.java
+++ src/main/java/br/com/astar/setupbox/domain/repository/AtivoRepository.java
@@ -1,9 +1,11 @@
 package br.com.astar.setupbox.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.astar.setupbox.domain.model.Ativo;
 
+@Repository
 public interface AtivoRepository extends JpaRepository<Ativo, Long> {
 
 }
