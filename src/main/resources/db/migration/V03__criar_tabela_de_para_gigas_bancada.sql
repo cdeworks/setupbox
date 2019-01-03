0aef14edf5c1325d1b82a2b61938172ea290bfd5
Daniel Reis <dreis2003@hotmail.com>
2019-01-03 17:19:15 -0200

    Flyway criação de tabela gigas e insert dos registros

---
+++ src/main/resources/db/migration/V03__criar_tabela_de_para_gigas_bancada.sql
@@ -0,0 +1,7 @@
+CREATE TABLE de_para_gigas_bancada (
+	id 				BIGINT(20) 		PRIMARY KEY AUTO_INCREMENT,
+	diagnostico		VARCHAR(300) ,
+	tipo			VARCHAR(15),
+	gigas		VARCHAR(10) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	
