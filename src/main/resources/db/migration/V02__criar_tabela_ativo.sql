f40e8eccb8cc4b0f83c5d6b2a1a17092fcbab48b
Daniel Reis <dreis2003@hotmail.com>
2019-01-04 14:49:53 -0200

    Ajuste no arquivo de migração (edentações)

--- src/main/resources/db/migration/V02__criar_tabela_ativo.sql
+++ src/main/resources/db/migration/V02__criar_tabela_ativo.sql
@@ -1,15 +1,18 @@
 CREATE TABLE ativos (
-	id 				BIGINT(20) 		PRIMARY KEY AUTO_INCREMENT,
-	tipo			VARCHAR(50),
-	modelo			VARCHAR(50),
-	fabricante		VARCHAR(50), 
-	serial_number	VARCHAR(50),
-	cad_id			VARCHAR(50),
-	cm_mac			VARCHAR(50),
-	emta_mac		VARCHAR(50),
-	estado			VARCHAR(50),
-	tipo_defeito	VARCHAR(50),
-	localizacao		VARCHAR(50)
+	id 					BIGINT(20) 		PRIMARY KEY AUTO_INCREMENT,
+	tipo				VARCHAR(50),
+	modelo				VARCHAR(50),
+	fabricante			VARCHAR(50), 
+	serial_number		VARCHAR(50),
+	cad_id				VARCHAR(50),
+	cm_mac				VARCHAR(50),
+	emta_mac			VARCHAR(50),
+	estado				VARCHAR(50),
+	tipo_defeito		VARCHAR(50),
+	localizacao			VARCHAR(50),
+	status				VARCHAR(10),
+	data_importacao 	DATE,
+	data_processamento 	DATE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
