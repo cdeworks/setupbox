d461d7df394a08c5b302f6f3dfc0c1c38d493594
Daniel Reis <dreis2003@hotmail.com>
2019-01-07 15:17:55 -0200

    Inclusão de metodo para busca pelo serial number

--- src/main/java/br/com/astar/setupbox/domain/repository/AtivoRepository.java
+++ src/main/java/br/com/astar/setupbox/domain/repository/AtivoRepository.java
@@ -1,5 +1,7 @@
 package br.com.astar.setupbox.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.astar.setupbox.domain.model.Ativo;
 
 @Repository
 public interface AtivoRepository extends JpaRepository<Ativo, Long> {
+	
+	Optional<Ativo> findBySerialNumber(String serialNumber);
 
 }
