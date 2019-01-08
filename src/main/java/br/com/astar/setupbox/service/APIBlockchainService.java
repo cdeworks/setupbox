03004164591ee7c2b2d7142298eb96a126630800
Daniel Reis <dreis2003@hotmail.com>
2019-01-07 22:56:10 -0200

    Classe de serviço que ficará responsavel pelo envio a Blockchain

--- src/main/java/br/com/astar/setupbox/service/APIBlockchainService.java
+++ src/main/java/br/com/astar/setupbox/service/APIBlockchainService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.astar.setupbox.domain.enums.StatusArquivo;
 import br.com.astar.setupbox.domain.enums.TipoArquivoImportacao;
 import br.com.astar.setupbox.domain.model.Ativo;
 import br.com.astar.setupbox.domain.model.Evento;
@@ -20,46 +21,62 @@ public class APIBlockchainService {
 	private AtivoRepository ativoRepository;
 	
 	
-	public void enviar(Ativo ativo, TipoArquivoImportacao tipoArquivo) {
+	public void enviar(Ativo ativo, TipoArquivoImportacao tipoArquivo, String status) {
 		
 		String login = "";
 		String senha = "";
 		String uri = "";
 		
-		
-		
-		
 		RESTClient client = new RESTClient(login, senha, uri);
+		
+		Optional<Ativo> atv = null;
 		
 		
 		switch(tipoArquivo) {
 		
 		case LABORATORIO:
-			client.request(uri, HTTPMethod.GET, criarEvento(ativo, tipoArquivo));
+			client.request(uri, HTTPMethod.GET, criarEvento(ativo, tipoArquivo, status));
+			atv = ativoRepository.findById(ativo.getId());
+			atv.get().setStatus(StatusArquivo.ENVIADO);
+			atv.get().setDataProcessamento(LocalDateTime.now());;
+			ativoRepository.save(atv.get());
 			break;
 		case EMBALAGEM:
-			// client.request(uri, HTTPMethod.GET, criarEvento(ativo, tipoArquivo));
-			Optional<Ativo> atv = ativoRepository.findBySerialNumber(ativo.getSerialNumber());
+			client.request(uri, HTTPMethod.GET, criarEvento(ativo, tipoArquivo,status));
+			atv = ativoRepository.findBySerialNumber(ativo.getSerialNumber());
 			if (atv.isPresent()) {
 				atv.get().setLocalizacao(tipoArquivo.name());
 				atv.get().setDataProcessamento(LocalDateTime.now());
+				atv.get().setStatus(StatusArquivo.ENVIADO);
 				ativoRepository.save(atv.get());
-				
 			}
+			break;
+		case HIGIENIZACAO:
+			client.request(uri, HTTPMethod.GET, criarEvento(ativo, tipoArquivo,status));
+			atv = ativoRepository.findBySerialNumber(ativo.getSerialNumber());
+			if (atv.isPresent()) {
+				atv.get().setLocalizacao(tipoArquivo.name());
+				atv.get().setDataProcessamento(LocalDateTime.now());
+				atv.get().setStatus(StatusArquivo.ENVIADO);
+				ativoRepository.save(atv.get());
+			}
+			break;
+			
+			
+		default:
 			break;
 		
 		}
 		
-		
-		
 	}
 	
 	
-	private Evento criarEvento(Ativo ativo, TipoArquivoImportacao tipoArquivo) {
+	private Evento criarEvento(Ativo ativo, TipoArquivoImportacao tipoArquivo, String status) {
 		Evento evento = new Evento();
 		evento.setSerialNumber(ativo.getSerialNumber());
 		evento.setLocalizacao(tipoArquivo.name());
 		evento.setDataHora(LocalDateTime.now());
+		evento.setStatus(status);
 		return evento;
 	}
 	
